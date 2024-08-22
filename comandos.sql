1fdfdc3af70c7acdd25afd42f85a5f4f72a67e7d
Jhonzin1221 <jhonatanmoura1221@gmail.com>
2024-08-22 09:31:39 -0500

    att

---
+++ comandos.sql
@@ -0,0 +1,10 @@
+-- comandos para criar database(banco de dados)
+CREATE DATABASE senai;
+
+-- comandos para criar tabela
+CREATE TABLE Alunos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255)
+);
+
