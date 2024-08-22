7038a8ddf611329c755cca5704c688c9a1e7c569
Jhonzin1221 <jhomatanmoura1221@gmail.com>
2024-08-22 13:58:11 -0500

    att2

--- comandos.sql
+++ comandos.sql
@@ -8,3 +8,13 @@ CREATE TABLE Alunos(
     email VARCHAR(255)
 );
 
+-- alterar nome da tabela
+ALTER TABLE alunos
+CHANGE nnome_pessoal VARCHAR(255);
+
+-- mudar nome de tabela
+RENAME TABLE aluno TO alunos;
+
+-- adicionando campo após tabela criada
+ALTER TABLE alunos
+ADD descrição VARCHAR(255);
