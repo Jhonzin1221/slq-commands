849b70f6d6c99259e413c5b75562cd4ac0468ef0
Jhonzin1221 <jhonatanmoura1221@gmail.com>
2024-08-23 13:52:07 -0500

    att3

--- comandos.sql
+++ comandos.sql
@@ -18,3 +18,23 @@ RENAME TABLE aluno TO alunos;
 -- adicionando campo após tabela criada
 ALTER TABLE alunos
 ADD descrição VARCHAR(255);
+
+-- Atualização de Multiplos registros
+UPDATE Clientes
+SET Nome = 'Cliente VIP'
+WHERE Idade > 30;
+
+--comando para inserir dados em uma tabela
+INSERT INTO alunos (nome, email)
+VALUES ('joao','j@gmail.com');
+
+-- deletar usuario
+DELETE FROM Alunos WHERE id = 1;
+
+            --selecionar colunas especificas
+SELECT nome, email FROM alunos; --seleciona nome e email dos alunos
+SELECT nome, email FROM alunos WHERE idade > 30; --seleciona pessoas com mais de 30 anos
+SELECT nome, email FROM alunos WHERE email = 'rg@gmail.com'; --selecionar um usuario especifico
+
+SELECT * FROM alunos WHERE nome = 'nome' AND senha = '1234'
+SELECT * FROM alunos WHERE nome = 'nome' OR senha = '1234'
